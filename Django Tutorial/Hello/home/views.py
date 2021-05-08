e74c669885bbdb6da8ef17d0d1db4a8f4e9f21b1
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-08 20:19:23 +0530

    Added contact us form

--- Django Tutorial/Hello/home/views.py
+++ Django Tutorial/Hello/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 def index(request):
     # return HttpResponse("This is Homepage")
@@ -14,4 +17,12 @@ def services(request):
     return render(request, 'services.html')
  
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
     return render(request, 'contact.html')
