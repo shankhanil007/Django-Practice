e0f5044e464270af5d1257acb82ff5607cfc8981
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-09 18:08:58 +0530

    Displaying items

--- Django Tutorial/mycart/shop/views.py
+++ Django Tutorial/mycart/shop/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from .models import Product
+from math import ceil
+
+# Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-   return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return HttpResponse("We are at about")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("We are at contact")
