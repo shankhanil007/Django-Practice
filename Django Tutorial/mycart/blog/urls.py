b72e301a0d538bcb78067552e38d5ad7d1fa7dcb
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-09 22:59:28 +0530

    created absic blog app

--- Django Tutorial/mycart/blog/urls.py
+++ Django Tutorial/mycart/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="blogHome"),
+    path("blogpost/", views.blogpost, name="blogHome")
 ]
--- Django Tutorial/mycart/blog/views.py
+++ Django Tutorial/mycart/blog/views.py
@@ -1,5 +1,11 @@
+
 from django.shortcuts import render
+
+# Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,"blog/index.html")
+    return render(request, 'blog/index.html')
+
+def blogpost(request):
+    return render(request, 'blog/blogpost.html')
