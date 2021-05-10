ef6876a7ecc2aa72bda35bc84715d3ec3d69d41b
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-10 10:13:57 +0530

    added messages in django

--- Django Tutorial/icoder/icoder/settings.py
+++ Django Tutorial/icoder/icoder/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from django.contrib.messages import constants as messages
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig', 
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
