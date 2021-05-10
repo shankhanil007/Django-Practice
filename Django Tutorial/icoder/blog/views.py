1f3e9a2c759ab9aa80c3967d2980c11dacf51e0a
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-10 10:21:47 +0530

    Added HTML CSS and JS codes before authentication

--- Django Tutorial/icoder/blog/views.py
+++ Django Tutorial/icoder/blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def blogHome(request): 
-    return render(request, 'blog/blogHome.html')
+    allPosts= Post.objects.all()
+    context={'allPosts': allPosts}
+    return render(request, "blog/blogHome.html", context)
 
 def blogPost(request, slug): 
     # return HttpResponse(f'This is blogPost : {slug}')
--- Django Tutorial/icoder/home/models.py
+++ Django Tutorial/icoder/home/models.py
@@ -12,3 +12,6 @@ class Contact(models.Model):
 
      def __str__(self):
           return "Message from " + self.name + ' - ' + self.email
+
+
+
