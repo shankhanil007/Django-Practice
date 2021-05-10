10dd0ff6b6a78f39157e880e6e318cfffa72f668
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-10 11:57:27 +0530

    Added new model

--- Django Tutorial/icoder/blog/models.py
+++ Django Tutorial/icoder/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
+
 class Post(models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=255)
@@ -9,6 +10,7 @@ class Post(models.Model):
     slug=models.CharField(max_length=130)
     timeStamp=models.DateTimeField(blank=True)
     content=models.TextField()
+
 
     def __str__(self):
         return self.title + " by " + self.author
@@ -20,3 +22,7 @@ class BlogComment(models.Model):
     post=models.ForeignKey(Post, on_delete=models.CASCADE)
     parent=models.ForeignKey('self',on_delete=models.CASCADE, null=True )
     timestamp= models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.comment[0:13] + "..." + "by" + " " + self.user.username
+    
