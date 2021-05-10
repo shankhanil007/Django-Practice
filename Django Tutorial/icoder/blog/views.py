f6de0d665abdb8f129ecf66f92a4cccbe8966fea
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-10 09:35:38 +0530

    added template files

--- Django Tutorial/icoder/blog/views.py
+++ Django Tutorial/icoder/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def blogHome(request): 
-    return HttpResponse('This is  blog home. We will keep all blog posts here')
+    return render(request, 'blog/blogHome.html')
 
 def blogPost(request, slug): 
-    return HttpResponse(f'This is blogPost : {slug}')
+    # return HttpResponse(f'This is blogPost : {slug}')
+    return render(request, 'blog/blogPost.html')
