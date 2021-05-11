98295675b981442e729141d911d28b7989627070
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-11 08:49:02 +0530

    Added scrapper code

---
+++ Google_News/main.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+def request(url):
+    res = urlopen(url)
+    data = res.read()
+    res.close()
+    soup = BeautifulSoup(data, 'lxml')            # lxml and xml diff
+    return soup
+
+urls = ['https://www.indiatoday.in/top-stories']
+soup = request(urls[0])
+
+temp = soup.find_all('div',{'class':'catagory-listing'})
+articles_list = []
+
+for news in temp:
+    images = news.find('img')['src']
+    title = news.find('a').text
+    story_url = 'https://www.indiatoday.in'+news.find('a')['href']
+    brief = news.find('p').text
+    wd = ""
+    for word in brief:
+        if(word!='\n'):
+            wd+=word
+    brief = wd
+
+    soup2 = request(story_url)
+    date = soup2.find('dt',{'class':'pubdata'}).text
+
+    articles_list.append([title, story_url, brief, images])
---
+++ Google_News/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(articles_list)
