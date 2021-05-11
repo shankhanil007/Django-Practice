39f63f747b94bd814a2c78a5e28effb373cd6e88
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-11 21:27:27 +0530

    Added PayTM payment integration

--- Django Tutorial/mycart/shop/views.py
+++ Django Tutorial/mycart/shop/views.py
@@ -3,11 +3,10 @@ from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from PayTm import Checksum
+from PayTm import Checksum
 # Create your views here.
 from django.http import HttpResponse
-MERCHANT_KEY = 'Your-Merchant-Key-Here'
-
+MERCHANT_KEY = ''
 
 def index(request):
     allProds = []
@@ -20,31 +19,6 @@ def index(request):
         allProds.append([prod, range(1, nSlides), nSlides])
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
-
-def searchMatch(query, item):
-    '''return true only if query matches the item'''
-    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
-        return True
-    else:
-        return False
-
-def search(request):
-    query = request.GET.get('search')
-    allProds = []
-    catprods = Product.objects.values('category', 'id')
-    cats = {item['category'] for item in catprods}
-    for cat in cats:
-        prodtemp = Product.objects.filter(category=cat)
-        prod = [item for item in prodtemp if searchMatch(query, item)]
-
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        if len(prod) != 0:
-            allProds.append([prod, range(1, nSlides), nSlides])
-    params = {'allProds': allProds, "msg": ""}
-    if len(allProds) == 0 or len(query)<4:
-        params = {'msg': "Please make sure to enter relevant search query"}
-    return render(request, 'shop/search.html', params)
 
 
 def about(request):
@@ -75,14 +49,18 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
+                    response = json.dumps([updates, order[0].items_json], default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{"status":"noitem"}')
+                return HttpResponse('{}')
         except Exception as e:
-            return HttpResponse('{"status":"error"}')
+            return HttpResponse('{}')
 
     return render(request, 'shop/tracker.html')
+
+
+def search(request):
+    return render(request, 'shop/search.html')
 
 
 def productView(request, myid):
@@ -114,7 +92,7 @@ def checkout(request):
         # Request paytm to transfer the amount to your account after payment by user
         param_dict = {
 
-                'MID': 'Your-Merchant-Id-Here',
+                'MID': '',
                 'ORDER_ID': str(order.order_id),
                 'TXN_AMOUNT': str(amount),
                 'CUST_ID': email,
